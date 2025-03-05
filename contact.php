c6e8a4b102f0a2a9938780417d695f9e6ada4f35
Keo Ratana <162092337+ratanakeo2024@users.noreply.github.com>
2025-03-05 13:44:48 +0700

    My update

---
+++ contact.php
@@ -0,0 +1,48 @@
+
+<section id="billboard" class="position-relative overflow-hidden bg-light-blue">
+<div class="container-fluid" style="margin-top: 150px; margin-bottom:150px;">
+
+	<div class="row">
+		<div class="col-12">
+			<h4 class="display-4 text-center">Contact US</h4>
+			<hr class="my-4">
+		</div>
+
+		<div class="col-xs-12 col-md-6 " id="contact">
+
+			<form class="mb-sm-4" id="for">
+
+				<div class="form-group">
+					<label for="exampleFormControlInput2">Name</label>
+					<input type="name" class="form-control mt-4 mb-4" id="exampleFormControlInput2" placeholder="User Name">
+				</div>
+
+
+				<div class="form-group">
+					<label for="exampleFormControlInput1">Email address</label>
+					<input type="email" class="form-control mt-4 mb-4" id="exampleFormControlInput1"
+                        placeholder="name@example.com">
+				</div>
+
+				<div class="form-group">
+					<label for="exampleFormControlTextarea1">Message</label>
+					<textarea class="form-control mt-4" id="exampleFormControlTextarea1" rows="3"></textarea>
+				</div>
+
+				<button type="submit" class="btn btn-info mt-4">Submit</button>
+			</form>
+
+		</div>
+
+		<div class="col-xs-12 col-md-6" id="contact">
+			<iframe
+				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115806.34359550575!2d91.79094155131145!3d24.899747201107232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375054d3d270329f%3A0xf58ef93431f67382!2sSylhet!5e0!3m2!1sen!2sbd!4v1516883061409"
+				width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+
+		</div>
+
+
+	</div>
+
+</div>
+</section>
